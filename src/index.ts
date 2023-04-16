c62730620d89702d38f58a068a02977e88397d13
Abraao Matos <abraao.matos236@gmail.com>
2023-04-15 20:13:22 -0400

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world!");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("hello gostoso");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
